34430402576cc79010889120a5e62ba9f068f4db
Hrach92 <hrach.gh.92@mail.ru>
2022-06-13 21:19:48 +0400

    ssr

--- pages/search/index.js
+++ pages/search/index.js
@@ -7,6 +7,11 @@ function Result({filter}){
     const [search,setSearch]=useState('')
     useEffect( ()=>{
         fetch(`https://api.themoviedb.org/3/search/movie?api_key=c1fe0a5e08d1bf80be453d4b47b27699&language=en-US&query=${filter}`).then(e=>{
+            if(!e){
+                return {
+                    notFound:true
+                }
+            }
             return e.json()
         }).then(e=>{
             
@@ -16,7 +21,6 @@ function Result({filter}){
     return search?<div className={movie.container}>
         {console.log('h')}
         {search.map(({title,id,poster_path})=>{
-            debugger
             return <Link key={id} href={`/movies/${id}`}><div className={movie.cell}><a><img className={movie.image} src={`https://image.tmdb.org/t/p/w342${poster_path}`}/>{title}</a></div></Link>
         })}
     </div>:'Not Found'
